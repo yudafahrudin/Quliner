c5a09f4ade2206d27d93ba5b5c1b3a7b605dd7e8
yudafahrudin <yuda@twiscode.com>
2018-12-09 16:41:59 +0700

    Setting database

--- config/database.php
+++ config/database.php
@@ -7,8 +7,6 @@ $username = $url["user"];
 $password = $url["pass"];
 $database = substr($url["path"], 1);
 
-die($host);
-
 return [
 
     /*
@@ -66,7 +64,6 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => $host,
-            'port' => env('DB_PORT', '5432'),
             'database' => $database,
             'username' => $username,
             'password' => $password,
